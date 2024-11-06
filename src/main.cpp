b97e9018e6d24055dce9e4c9fad0c307bc547ef9
Nate Wong <natewong1@gmail.com>
2024-11-06 12:44:14 -0500

    Add third party httplib to include, add it to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "httplib.h"
+httplib::Server svr;
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    std::cout << "its working" << std::endl;
+    svr.Get("/", [](const httplib::Request &, httplib::Response &res)
+            { res.set_content("Hello World!", "text/plain"); });
+    svr.listen("0.0.0.0", 8080);
     return 0;
 }
