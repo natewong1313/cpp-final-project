8547560155a6738f0e4a2672c87a4dbce94c15da
Nate Wong <natewong1@gmail.com>
2024-11-07 13:09:59 -0500

    update schema with a users table and fks

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,15 @@
+
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS messages(
-    id INTEGER,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
     content TEXT,
-    created_at INTEGER
+    created_at INTEGER,
+    FOREIGN KEY (author_id) REFERENCES users(id)
 );
