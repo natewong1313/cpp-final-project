5bc5d554ac734be34f5e388ddf84f700da752270
Nate Wong <natewong1@gmail.com>
2024-11-07 10:53:18 -0500

    move private vars to top of class

--- src/server.cpp
+++ src/server.cpp
@@ -3,19 +3,27 @@
 using namespace std;
 
 class Server {
-  public:
-    Server();
-
   private:
+    int id;
+    int admin_id;
     string name;
     vector<Message> messages;
+
+  public:
+    Server(int admin_id, string name) {
+      this->admin_id = admin_id;
+      this->name = name;
+    }
 };
 
 class Message {
-  public:
-    Message(string content, string authorId);
-
   private:
+    int id;
     string content;
     string authorId;
+
+  public:
+    Message(string content, string authorId);
+    void update(string newContent) {};
+    void save();
 };
