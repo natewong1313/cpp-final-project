8844195e57fa38167ca035984c2afde93c7235fe
Nate Wong <natewong1@gmail.com>
2024-11-08 13:12:16 -0500

    Use uuid for all ids

--- sql/init.sql
+++ sql/init.sql
@@ -1,13 +1,13 @@
 
 CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id TEXT PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS servers(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id TEXT PRIMARY KEY,
     admin_id INTEGER NOT NULL,
     name TEXT NOT NULL,
     FOREIGN KEY (admin_id) REFERENCES users(id)
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS servers(
 
 CREATE TABLE IF NOT EXISTS messages(
     id TEXT PRIMARY KEY,
-    author_id INTEGER NOT NULL,
-    server_id INTEGER NOT NULL,
+    author_id TEXT NOT NULL,
+    server_id TEXT NOT NULL,
     content TEXT,
     created_at INTEGER,
     FOREIGN KEY (author_id) REFERENCES users(id),
