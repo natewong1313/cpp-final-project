a11c48473bf0bdddd919245a87027b0870eafacc
Nate Wong <natewong1@gmail.com>
2024-11-21 15:34:36 -0500

    30 minutes of debugging because I didnt import the fucking string library

--- src/services/messagemanager.cpp
+++ src/services/messagemanager.cpp
@@ -3,19 +3,39 @@
 #include <iostream>
 #include <mutex>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
-MessageManager::MessageManager() {}
+MessageManager::MessageManager() {
+  channels = unordered_map<string, shared_ptr<ChannelListener>>();
+}
 
 void MessageManager::broadcast_message(string channelId, string message) {
-  lock_guard<mutex> lk(m);
-  cv.notify_all();
-  cout << "message broadcasted" << endl;
+  // lock_guard<mutex> lk(m);
+  // cv.notify_all();
+  // cout << "message broadcasted" << endl;
+}
+
+shared_ptr<ChannelListener> MessageManager::get_listener(string channelId) {
+  // shared lock which defers unlocking
+  shared_lock<shared_mutex> lock(m);
+  auto listener = channels.find(channelId);
+  if (listener == channels.end()) { return nullptr; }
+  return listener->second;
 }
 
 void MessageManager::listen_for_message(string channelId) {
   cout << "Listening" << endl;
-  unique_lock<mutex> lk(m);
-  cv.wait(lk);
-  cout << "Message received" << endl;
+
+  auto listener = get_listener(channelId);
+  if (listener == nullptr) {
+    cout << "Channel not found" << endl;
+    return;
+  } else {
+    cout << "Channel found" << endl;
+  }
+
+  // unique_lock<mutex> lk(m);
+  // cv.wait(lk);
+  // cout << "Message received" << endl;
 }
--- src/services/messagemanager.h
+++ src/services/messagemanager.h
@@ -2,15 +2,28 @@
 #define MESSAGEMANAGER_H
 
 #include <condition_variable>
-#include <map>
 #include <mutex>
+#include <shared_mutex>
+#include <string>
+#include <unordered_map>
 using namespace std;
+
+class ChannelListener {
+  private:
+    condition_variable cv;
+    mutex m;
+
+  public:
+    ChannelListener();
+};
 
 class MessageManager {
   private:
     condition_variable cv;
-    mutex m;
-    // map<string, ChannelListener> channels;
+    shared_mutex m;
+    unordered_map<string, shared_ptr<ChannelListener>> channels;
+
+    shared_ptr<ChannelListener> get_listener(string channelId);
 
   public:
     MessageManager();
